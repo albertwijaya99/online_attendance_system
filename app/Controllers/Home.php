e11a01bd29b9e36ce63a8937269f275f3eb6b304
Albert <albetz99@gmail.com>
2020-09-15 22:51:25 +0700

    removing test code

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,13 +4,6 @@ class Home extends BaseController
 {
 	public function index()
 	{
-	    $db = \Config\Database::connect();
-        if ($db->query("SELECT 1 AS one")) {
-            echo "a";
-        } else {
-            echo "b";
-        }
-
 		return view('welcome_message');
 	}
 
