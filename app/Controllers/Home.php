ae1b4be2b4539e578e9852c74213499c418da451
Albert <albetz99@gmail.com>
2020-09-15 21:43:33 +0700

    changed to production mode, add some code to test db connection

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,6 +4,13 @@ class Home extends BaseController
 {
 	public function index()
 	{
+	    $db = \Config\Database::connect();aasd
+        if ($db->query("SELECT 1 AS one")) {
+            echo "a";
+        } else {
+            echo "b";
+        }
+
 		return view('welcome_message');
 	}
 
