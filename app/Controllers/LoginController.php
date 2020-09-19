af6a0dcaa161422c7c5bc94604d058c9b4cf1340
nandaputriafifah <nanda@paola.id>
2020-09-20 00:40:14 +0700

    #21 Adding UI for login form

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -27,7 +27,8 @@ class LoginController extends Controller
             return redirect()->to(base_url('/profile'));
         }
         else{
-            return 'login gagal boi';
+            $loginErrorMsg = "Email or password incorrect";
+            return $loginErrorMsg;
         }
 
     }
