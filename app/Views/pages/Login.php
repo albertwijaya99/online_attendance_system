e4fe97ee7ce9819014798afa1be947718fcb564a
nandaputriafifah <nanda@paola.id>
2020-09-21 02:38:26 +0700

    fix login UI

--- app/Views/pages/Login.php
+++ app/Views/pages/Login.php
@@ -5,7 +5,15 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    
+
+    <!-- Bootstrap CSS - Show Password -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+    <!-- Bootstrap JS - Show Password -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-show-password@1.2.1/dist/bootstrap-show-password.min.js"></script>
+
     <!-- Custom Login CSS -->
     <link rel="stylesheet" type = "text/css"  href="<?php echo base_url('css/Login.css'); ?>">
     <title>Online Attendance System</title>
@@ -16,24 +24,22 @@
 <div class="container">
 <img src="<?php echo base_url('assets/images/docotel-logo.png'); ?>" class="img-docotel-logo">
 <img src="<?php echo base_url('assets/images/on-login.svg'); ?>" class="center img-vector">
-<h1>Online Attendance</h1>
+<h2>Online Attendance</h2>
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card card-signin my-5">
           <div class="card-body">
             <!-- <h5 class="card-title text-center">Sign In</h5> -->
             <form class="form-signin" action="<?= base_url('/checkLogin');?>" method="post">
+             <label for="loginEmail">Email address</label>
               <div class="form-label-group">
                 <input type="email" name="loginEmail" id="loginEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="loginEmail">Email address</label>
               </div>
 
+               <label for="loginPassword">Password</label>
               <div class="form-label-group">
                 <input type="password" name="loginPassword" id="loginPassword" class="form-control" placeholder="Password" required>
-                <label for="loginPassword">Password</label>
               </div>
-              <input type="checkbox" class="checkbox-show-password" onclick="Toggle()"> 
-              <label class="show-password">Show password</label>
               <button class="btn btn-lg btn-submit btn-block text-uppercase" type="submit">Login</button>
            </form>
           </div>
@@ -42,17 +48,10 @@
     </div>
   </div>
 
-  <script> 
-    // Change the type of input to password or text 
-        function Toggle() { 
-            var temp = document.getElementById("loginPassword"); 
-            if (temp.type === "password") { 
-                temp.type = "text"; 
-            } 
-            else { 
-                temp.type = "password"; 
-            } 
-        } 
-</script> 
+<script>
+  $(function() {
+    $('#loginPassword').password()
+  })
+</script>
 </body>
 
