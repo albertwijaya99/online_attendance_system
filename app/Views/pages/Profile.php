2bb647f16e7b5b8931ca6f180d427ee870af02f5
nandaputriafifah <nanda@paola.id>
2020-09-29 11:31:27 +0700

    add profile UI but not finished yet

--- app/Views/pages/Profile.php
+++ app/Views/pages/Profile.php
@@ -1,26 +1,49 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+ <!-- Custom Login CSS -->
+ <link rel="stylesheet" type = "text/css"  href="<?php echo base_url('css/Profile.css'); ?>">
 
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<a href="<?= base_url('/logout'); ?>">Logout</a>
+ <!-- Bootstrap JS -->
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<body>
 <br>
 
 <br>
-<?php if(isset($Employee)):?>
-    <?php foreach ($Employee as $item): ?>
-        <?php echo('Email       : '.$item['employeeEmail'])?>
-        <br>
-        <?php echo('Nama        : '.$item['employeeName'])?>
-        <br>
-        <?php echo('Division    : '.$item['division'])?>
-        <br>
-        <img onclick="ChangeProfilePicture()" src="<?php echo(base_url().'/Uploads/ProfilePicture/'.hash("sha512",session()->get('Email'))).'/'.$item['employeeImageUrl']?>" alt="profile_picture" name="employeeImageUrl" id="employeeImageUrl" width="200">
-        <br>
-    <?php endforeach;?>
-<?php endif;?>
+<div class="container">
+<div class="row">
+  <div class="col">
+  <img src="<?php echo base_url('assets/images/on-profile3.svg'); ?>" class="img-vector">
+  </div>
+  <div class="col-6 col-sm-5" style="padding-top: 55px">
+  <div class="card" style="width: 25rem; height: 30rem; border-radius: 25px;">
+    <div class="card-body">
+    <?php if(isset($Employee)):?>
+        <?php foreach ($Employee as $item): ?>
+         <div class="padding">
+          <div class="padding-bottom">
+         <img onclick="ChangeProfilePicture()" src="<?php echo(base_url().'/Uploads/ProfilePicture/'.hash("sha512",session()->get('Email'))).'/'.$item['employeeImageUrl']?>"
+         alt="profile_picture" name="employeeImageUrl" id="employeeImageUrl" class="img-profile">
+         </div>
+         <h5 class="card-title"><?php echo($item['employeeName'])?></h5>
+         <h6 class="card-subtitle mb-2 text-muted"> <?php echo($item['division'])?></h6>
+         <p class="card-text"> <?php echo($item['employeeEmail'])?></p>
+         </div>
+        <?php endforeach;?>
+    <?php endif;?>
+
+    <button class="btn-logout text-uppercase">
+    <a href="<?= base_url('/logout'); ?>" class="logout-text">Logout</a>
+    </button>
+    </div>
+  </div>
+  </div>
+</div>
+</div>
 <div class="modal" id="ChangeProfilePictureModal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -31,18 +54,19 @@
                 </button>
             </div>
             <div class="modal-body">
-<!--                Start of Change profile picture form-->
+            <!--Start of Change profile picture form-->
                 <form action="<?= base_url('changeProfilePicture')?>" enctype="multipart/form-data" method="post">
                     <input type="file" name="newProfilePicture" id="newProfilePicture" required>
             </div>
             <div class="modal-footer">
                 <button type="submit" class="btn btn-primary">Change</button>
                 </form>
-<!--                End of Change profile picture form-->
+                <!-- End of Change profile picture form -->
             </div>
         </div>
     </div>
 </div>
+</body>
 
 <script>
     console.log('Session : <?=session()->get("Email") ?>');
